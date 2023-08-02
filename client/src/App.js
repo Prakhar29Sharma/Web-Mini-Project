ebfefae12a07b2c65525dc004b52080b29cd3dbe
Dhaneshwari Tendle <110600266+dhaneshwaritendle@users.noreply.github.com>
2023-08-02 23:32:53 +0530

    reverting changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
+   Route,
   } from "react-router-dom";
 
 import Dashboard from './screens/Dashboard';
@@ -9,19 +9,60 @@ import Profile from './screens/Profile';
 import Course from './screens/Course';
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div>
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<Dashboard/>} />
-            <Route path="/Profile" element={<Profile/>} />
-            <Route path="/Course" element={<Course/>} />
-          </Routes>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} className="elevate:[4]"/>
+            <Router>
+            <Routes>
+             <Route path="/" element={<Dashboard/>} />
+             <Route path="/Profile" element={<Form/>} />
+             {/* <Route path="/Course" element={<Course/>} /> */}
+           </Routes>
+           </Router>
+          </main>
         </div>
-      </Router>
-    </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
+
+
+
+// function App() {
+//   const [theme, colorMode] = useMode();
+//   const [isSidebar, setIsSidebar] = useState(true);
+
+//   return (
+//     <ColorModeContext.Provider value={colorMode}>
+
+//     <ThemeProvider theme={theme}>
+//     <CssBaseline />
+//     <div className="app">
+//       <Sidebar/>
+//       <main className="content">
+//             <Topbar setIsSidebar={setIsSidebar} />
+//       <Router> 
+//         <div>
+//           <Routes>
+//             <Route path="/" element={<Dashboard/>} />
+//             <Route path="/Profile" element={<Form/>} />
+//             {/* <Route path="/Course" element={<Course/>} /> */}
+//           </Routes>
+//         </div>
+//       </Router>
+//       </main>
+//     </div>
+//     </ThemeProvider>
+//     </ColorModeContext.Provider>
+//   );
+// }
 
 export default App;
---
+++ client/src/screens/dashboard/Dashboard.js
@@ -0,0 +1,82 @@
+import React from 'react'
+// import Sidebar2 from '../global/Sidebar2'
+// import Navbars from '../global/Navbars'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid'; // Grid version 1
+import CardContent from '@mui/material/CardContent';
+import Card from '@mui/material/Card';
+import { Typography } from '@mui/material';
+import Stack from '@mui/material/Stack';
+
+
+export default function Dashboard() {
+  return (
+    <div>
+      {/* <Navbars /> */}
+      <Box height={50} />
+      <Box sx={{ display: 'flex' }}>
+        {/* <Sidebar2 /> */}
+
+
+        <Box component='main' sx={{ flexGrow: 1, p: 5 }}>
+          <h3>Dashboard</h3>
+          <Box height={30} />
+          <Stack
+              direction={{ xs: 'column', sm: 'row' }}
+              spacing={{ xs: 1, sm: 2, md: 4 }}
+            >
+        
+         
+          <Grid container spacing={10}>
+            
+              <Grid item xs={8}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                     Contributors 
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Display number of contributors
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+
+
+
+              <Grid item xs={8}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Evaluators
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Display number of Evaluators
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+
+              <Grid item xs={8}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Students
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Display number of Students
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            
+          </Grid>
+          </Stack>
+        </Box>
+
+      </Box>
+      
+
+    </div>
+  )
+}
