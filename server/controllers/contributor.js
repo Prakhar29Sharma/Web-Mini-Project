df725cb7e74472ff398bd7625490c166943e57dd
Prakhar Sharma <prakhar29sharma@gmail.com>
2023-08-31 20:56:57 +0530

    added contributor routes

---
+++ server/controllers/contributor.js
@@ -0,0 +1,55 @@
+const Contributor = require('../models/contributor');
+
+/* READ */
+
+const getContributor = async (req, res) => {
+    try {
+        const user = req.user;
+        if (user.role !== 'ADMIN' || user.role !== 'CONTRIBUTOR') {
+            throw 'Unauthorized access';
+        }
+        const { username } = req.params;
+        const contributor = await Contributor.findOne({ username: username });
+        res.json({
+            status: 'ok',
+            data: contributor
+        })
+    } catch (err) {
+        res.json({ status: 'error', error: err });
+    }
+};
+
+const getContributors = async (req, res) => {
+    try {
+        const user = req.user;
+        if (user.role !== 'ADMIN') {
+            throw 'Unauthorized access';
+        }
+        const contributors = await Contributor.find({});
+        res.json({
+            status: 'ok',
+            data: contributors
+        });
+    } catch (err) {
+        res.json({ status: 'error', error: err });
+    }
+};
+
+/* CREATE */
+
+const createContributor = async (req, res) => {
+    try {
+        const user = req.user;
+        if (user.role !== 'CONTRIBUTOR') {
+            throw 'Unauthorized access';
+        }
+        const contributor = new Contributor(req.body);
+        await contributor.save();
+        res.json({
+            status: 'ok',
+            message: 'Contributor profile created successfully',
+        });
+    } catch (err) {
+        res.json({ status: 'error', error: err });
+    }
+};
