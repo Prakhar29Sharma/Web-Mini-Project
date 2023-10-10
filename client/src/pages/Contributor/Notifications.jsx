085a6d355c5bdfbe202d759ef287606556c47433
Prakhar Sharma <prakhar29sharma@gmail.com>
2023-10-10 13:32:32 +0530

    changed formatting for notifications page

--- client/src/pages/Contributor/Notifications.jsx
+++ client/src/pages/Contributor/Notifications.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { getToken } from "../../utils/auth";
-import NotificationItem from "../../components/NotificationItem";
+import { Link } from "react-router-dom";
 
 export default function Notifications() {
 
@@ -27,15 +27,47 @@ export default function Notifications() {
 
     return (
         <main id="main" className="main">
-            <ul className="notifications">
-                            {
-              notifications.map((notification, index) => {
-                return (
-                  <NotificationItem key={index} title={notification.title} message={notification.message} time={notification.time} />
-                );
-              })
-            }
-            </ul>
+            <section class="section dashboard">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="row">
+                        <div className="card">
+                            <div className="filter">
+                            <Link className="icon" to="" data-bs-toggle="dropdown"><i className="bi bi-three-dots"></i></Link>
+                            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                                <li className="dropdown-header text-start">
+                                <h6>Filter</h6>
+                                </li>
+                                <li><Link className="dropdown-item" to="">Today</Link></li>
+                                <li><Link className="dropdown-item" to="">This Month</Link></li>
+                                <li><Link className="dropdown-item" to="">This Year</Link></li>
+                            </ul>
+                            </div>
+
+                            <div className="card-body pb-0">
+                            <h5 className="card-title">Notifications</h5>
+
+                            <div className="news">
+                                {
+                                    notifications.map((notification, index) => {
+                                        return (
+                                            <div style={{ marginBottom: '25px' }} key={index} className="post-item clearfix">
+                                                <img src="http://localhost:5000/assets/notification.jpeg" alt="notification"/>
+                                                <h4><Link to="">{notification.title}</Link></h4>
+                                                <p style={{ textAlign: 'left' }}>{notification.message}</p>
+                                                <p style={{ textAlign: 'left' }}>{notification.time}</p>
+                                            </div>
+                                        )
+                                    })
+                                }
+
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
