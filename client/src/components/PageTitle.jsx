c83c40bac44136ab1e6362c4f562fd168c772538
Prakhar Sharma <prakhar29sharma@gmail.com>
2023-10-30 23:40:43 +0530

    fixed page title bug

--- client/src/components/PageTitle.jsx
+++ client/src/components/PageTitle.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 export default function PageTitle(props) {
 
     const user = localStorage.getItem('user');
-    const role = JSON.parse(user).role;
+    const role = user ? JSON.parse(user).role : null;
 
     return (
         <div className="pagetitle">
             <h1>{props.title}</h1>
             <nav>
                 <ol className="breadcrumb">
-                <li className="breadcrumb-item"><Link to={`/${role.toLowerCase()}`}>Home</Link></li>
+                <li className="breadcrumb-item"><Link to={`/${role ? role.toLowerCase() : null}`}>Home</Link></li>
                 <li className="breadcrumb-item active">{props.title}</li>
                 </ol>
             </nav>
